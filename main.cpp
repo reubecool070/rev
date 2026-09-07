4907951664254e9389d589cdf00bde9c8fa71188
reubecool070 <reubenthapa070@gmail.com>
2026-09-07 21:44:02 +0545

    create directory for initialization

--- main.cpp
+++ main.cpp
@@ -13,10 +13,17 @@ int main(int argc, char* argv[]) {
     }
 
     fs::path dir_path = ".rev";
+    fs::path dir_object = "./rev/objects";
+    fs::path dir_ref = "./rev/refs";
+    
 
     if (string(argv[1]) == "init") {
         // create directory
+        fs::create_directory(dir_path);
+        fs::create_directory(dir_object);
+        fs::create_directory(dir_ref);
         cout << "Initialized rev directory";
+
     }
     return 0;
 }
